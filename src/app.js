995ae8c27cfa01a1491cb1790e8039ca4b0e69e2
Gourav Chaudhary <gouravsittam@gmail.com>
2025-02-22 00:04:03 +0530

    Refactor Express routes and update response structure for user endpoints

--- src/app.js
+++ src/app.js
@@ -1,18 +1,23 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send("Hello, Gourav!");
+app.get("/user", (req, res) => {
+  res.send({ firstname: "Gobu", lastname: "Chaudhary" });
 });
 
-app.get('/test', (req, res) => {
-    res.send("Hello, hello!");
+app.post("/user", (req, res) => {
+  res.send("User created successfully");
 });
 
-app.get('/server', (req, res) => {
-    res.send("Greetings from the server!");
+app.delete("/user", (req, res) => {
+  res.send("Delete user successfully");
+});
+
+//this will match all the HTTP methods API call to  /Test
+app.use("/test", (req, res) => {
+  res.send("Hello, hello!");
 });
 
 app.listen(7777, () => {
-    console.log("Server is successfully listening on port 7777.");
+  console.log("Server is successfully listening on port 7777.");
 });
