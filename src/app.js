589c6e7389a94db9930b7c65e6b5c481e5b97e67
Gourav Chaudhary <gouravsittam@gmail.com>
2025-02-24 23:45:46 +0530

    Implement user signup route and update user schema definition

--- src/app.js
+++ src/app.js
@@ -2,6 +2,20 @@ require("dotenv").config();
 const express = require("express");
 const connectionDB = require("./config/database");
 const app = express();
+const User = require("./models/user");
+
+app.post("/signup", async(req, res) => {
+  //creating a new instance of user model
+
+  const user = new User({
+    firstName: "Kunal",
+    lastName: "Pathak",
+    emailID: "kp@gmail.com",
+    password: "KP@12345",
+  });
+  await user.save();
+  res.send("User added successfully");
+});
 
 connectionDB()
   .then(() => {
