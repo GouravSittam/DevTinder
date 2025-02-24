b3d55043a1fd42175381eaf903358ff9a69f9c1f
Gourav Chaudhary <gouravsittam@gmail.com>
2025-02-24 23:18:20 +0530

    Add dotenv for environment variable management and update database connection logic

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
+require("dotenv").config();
 const express = require("express");
 const connectionDB = require("./config/database");
 const app = express();
 
 connectionDB()
   .then(() => {
-    console.log("Database connected successfully....");
+    console.log("Database connected successfully✅✅");
     app.listen(7777, () => {
-      console.log("Server is successfully listening on port 7777.");
+      console.log("Server is successfully listening on port 7777⭐⭐");
     });
   })
   .catch((err) => {
--- src/config/database.js
+++ src/config/database.js
@@ -1,15 +1,11 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 const connectionDB = async () => {
-  await mongoose.connect(
-    "mongodb+srv://gouravsittam:BixBie2C7oFv2j4Z@devspark.yx5wf.mongodb.net/devTinder"
-  );
+  await mongoose.connect(process.env.MONGODB_URL);
 };
 
-
 module.exports = connectionDB;
-
-
 
 // mongoose.connect(
 //   "mongodb+srv://gouravsittam:BixBie2C7oFv2j4Z@devspark.yx5wf.mongodb.net/"
