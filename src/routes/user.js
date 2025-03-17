5c15cca2d958fde9e3e9b1eef685018c65ad9f2b
Gourav Chaudhary <gouravsittam@gmail.com>
2025-03-17 23:34:24 +0530

    Add connection request retrieval endpoint and update schema reference

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,5 +1,27 @@
 const express = require("express");
+const { userAuth } = require("../middlewares/auth");
+const ConnectionReqModel = require("../models/connectionReq");
 
 const userRouter = express.Router();
+
+userRouter.get("/user/request/received", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+
+    const connectionRequests = await ConnectionReqModel.find({
+      toUserId: loggedInUser._id,
+      status: "interested",
+    }).populate("fromUserId", ["firstName", "lastName"]);
+
+    res.json({
+      message: "Data Fetched Successfully",
+      data: connectionRequests,
+    });
+  } catch (err) {
+    res
+      .status(400)
+      .send("Error while fetching connection requests: " + err.message);
+  }
+});
 
 module.exports = userRouter;
