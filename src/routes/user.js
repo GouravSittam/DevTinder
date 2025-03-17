c7cef67a21491b70e5a0a5be4b583a5d07226eee
Gourav Chaudhary <gouravsittam@gmail.com>
2025-03-18 00:38:22 +0530

    Add endpoint for retrieving user connections and refactor data population

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,6 +3,14 @@ const { userAuth } = require("../middlewares/auth");
 const ConnectionReqModel = require("../models/connectionReq");
 
 const userRouter = express.Router();
+const USER_SAFE_DATA = [
+  "firstName",
+  "lastName",
+  "age",
+  "gender",
+  "about",
+  "skills",
+];
 
 userRouter.get("/user/request/received", userAuth, async (req, res) => {
   try {
@@ -11,23 +19,34 @@ userRouter.get("/user/request/received", userAuth, async (req, res) => {
     const connectionRequests = await ConnectionReqModel.find({
       toUserId: loggedInUser._id,
       status: "interested",
-    }).populate("fromUserId", [
-      "firstName",
-      "lastName",
-      "age",
-      "gender",
-      "about",
-      "skills",
-    ]);
+    }).populate("fromUserId", USER_SAFE_DATA);
 
     res.json({
       message: "Data Fetched Successfully",
       data: connectionRequests,
     });
   } catch (err) {
-    res
-      .status(400)
-      .send("Error while fetching connection requests: " + err.message);
+    res.status(400).send("Error while fetching connection requests: " + err.message);
+  }
+});
+
+userRouter.get("/user/connections", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+    const connectionRequests = await ConnectionReqModel.find({
+      $or: [
+        { fromUserId: loggedInUser._id, status: "accepted" },
+        { toUserId: loggedInUser._id, status: "accepted" },
+      ],
+    }).populate("fromUserId", USER_SAFE_DATA);
+
+    const data = connectionRequests.map((row) => row.fromUserId);
+    res.json({
+      message: "Data Fetched Successfully",
+      data: data,
+    });
+  } catch (err) {
+    res.status(400).send("Error while fetching connections: " + err.message);
   }
 });
 
