e8b7c4b8ca1a4a499436ab55e2becac0ee256188
Gourav Chaudhary <gouravsittam@gmail.com>
2025-03-25 17:40:37 +0530

    Add Redux store configuration and user slice for state management

--- DevSpark-Web/src/App.jsx
+++ DevSpark-Web/src/App.jsx
@@ -3,19 +3,21 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Body from "./Body";
 import Login from "./Login";
 import Profile from "./Profile";
+import { Provider } from "react-redux";
+import appStore from "./utils/appStore";
 
 const App = () => {
   return (
-    <div>
-      <BrowserRouter basename="/">
+    <Provider store={appStore}>
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Body />}>
-            <Route path="/login" element={<Login />} />
-            <Route path="/profile" element={<Profile />} />
+            <Route path="login" element={<Login />} />
+            <Route path="profile" element={<Profile />} />
           </Route>
         </Routes>
       </BrowserRouter>
-    </div>
+    </Provider>
   );
 };
 
