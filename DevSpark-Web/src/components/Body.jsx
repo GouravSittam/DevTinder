0b1994f3c6579d82a1f471f064ac6e3133c778ae
Gourav Chaudhary <gouravsittam@gmail.com>
2025-03-31 17:30:55 +0530

    Fetch user data on component mount and handle authentication; update .gitignore to include apiList.md

--- DevSpark-Web/src/components/Body.jsx
+++ DevSpark-Web/src/components/Body.jsx
@@ -1,15 +1,40 @@
-import React from "react";
+import React, { useEffect } from "react";
 import NavBar from "./NavBar";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import Footer from "./Footer";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import axios from "axios"; 
 
 const Body = () => {
+  const dispatch = useDispatch(); 
+  const navigate = useNavigate();
+
+  const fetchUser = async () => {
+    try {
+      const res = await axios.get(BASE_URL + "/profile/view", {
+        withCredentials: true,
+      });
+      dispatch(addUser(res.data)); 
+    } catch (err) {
+      console.error(err);
+      // Navigate to login if the user is not authenticated
+      navigate("/login");
+    }
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
   return (
     <div>
       <NavBar />
       <Outlet />
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 };
+
 export default Body;
