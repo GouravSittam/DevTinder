13c51ba80f7d06cff37da76056c8ca3a5f3c6fe0
Gourav Chaudhary <gouravsittam@gmail.com>
2025-04-06 00:00:25 +0530

    Add Requests component, update App routing, and integrate Redux for connection requests

--- DevSpark-Web/src/utils/appStore.js
+++ DevSpark-Web/src/utils/appStore.js
@@ -2,13 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
 import feedReducer from "./feedSlice";
 import connectionsReducer from "./connectionsSlice";
+import requestReducer from "./requestSlice";
 
 const store = configureStore({
   reducer: {
     user: userReducer,
     feed: feedReducer,
     connections: connectionsReducer,
-    
+    requests: requestReducer,
   },
 });
 
